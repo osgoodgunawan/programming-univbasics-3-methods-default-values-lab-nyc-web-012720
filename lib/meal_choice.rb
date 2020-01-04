fe38b86b38018b7e48db2d904c968d171310019e
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-04 20:19:49 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -9,7 +9,8 @@
 # end
 
 def meal_choice(veg1,veg2,protein="meat")
-  return  puts "What a nutritious meal! A plate of #{protein} with #{veg1} and #{veg2}"
+    puts "What a nutritious meal!"
+    puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
 meal_choice("broccoli","macaroni","tofu")
